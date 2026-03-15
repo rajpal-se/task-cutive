5570688efd4b472ccf08e648154d090c7f3896fa
Rajpal Singh <rajpal426a@gmail.com>
2026-03-15 12:23:55 +0530

    MUI Theme added.

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.module.scss";
-import App from "./app";
 
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
 import { RouterProvider } from "react-router";
 import router from "./router";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <CssBaseline />
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
     </StrictMode>,
 );
---
+++ src/theme/index.tsx
@@ -0,0 +1,42 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#8e24aa",
+            contrastText: "#fff",
+        },
+        secondary: {
+            main: "#00a800",
+            contrastText: "#fff",
+        },
+        background: {
+            default: "#f5f5f5",
+        },
+    },
+    typography: {
+        fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+        h4: {
+            fontFamily: '"Dancing Script", "Cursive"',
+        },
+    },
+    components: {
+        MuiTextField: {
+            defaultProps: {
+                variant: "outlined",
+                color: "primary",
+            },
+        },
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: 4,
+                    textTransform: "none",
+                    fontWeight: 600,
+                },
+            },
+        },
+    },
+});
+
+export default theme;

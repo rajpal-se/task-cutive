f78ede8f273593f549bf68d99283b170e1b5d8b7
Rajpal Singh <rajpal426a@gmail.com>
2026-03-15 16:37:04 +0530

    cong and env vars added.

---
+++ src/types/vite-env.d.ts
@@ -0,0 +1,11 @@
+interface ViteTypeOptions {
+    strictImportMetaEnv: unknown;
+}
+
+interface ImportMetaEnv {
+    readonly VITE_APP_NAME: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}

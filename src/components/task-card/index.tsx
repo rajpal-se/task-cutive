95a36dd06389eef9c51731929a5ab218cca215b7
Rajpal Singh <rajpal426a@gmail.com>
2026-03-20 17:22:44 +0530

    console error fixed.

--- src/components/task-card/index.tsx
+++ src/components/task-card/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, styled, Typography } from "@mui/material";
+import { Box, Divider, styled } from "@mui/material";
 import CalenderCard from "./calender-card";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
@@ -9,13 +9,13 @@ interface TaskCardProps {
     description: string;
     dueDate: Date;
     createdAt: Date;
-    isCompeleted: boolean;
+    isCompleted: boolean;
 }
 
 export default function TaskCard(props: TaskCardProps) {
-    const { title, description, dueDate, createdAt, isCompeleted } = props;
+    const { title, description, dueDate, createdAt, isCompleted } = props;
     return (
-        <TaskCardContainer isCompeleted={isCompeleted}>
+        <TaskCardContainer isCompleted={isCompleted}>
             <Box className="calender-card">
                 <CalenderCard date={dueDate} />
             </Box>
@@ -33,7 +33,7 @@ export default function TaskCard(props: TaskCardProps) {
                 <div className="dates">
                     <p>Remaining: 10 mins</p>
                     <Divider orientation="vertical" />
-                    <p>Added on: {dueDate.toLocaleString()}</p>
+                    <p>Added on: {createdAt.toLocaleString()}</p>
                 </div>
             </Box>
             <Box className="task-actions">
@@ -50,111 +50,111 @@ export default function TaskCard(props: TaskCardProps) {
     );
 }
 
-const TaskCardContainer = styled(Box)<{ isCompeleted: boolean }>(
-    ({ isCompeleted = false }) => ({
+const TaskCardContainer = styled(Box, {
+    shouldForwardProp: (prop) => prop !== "isCompleted",
+})<{ isCompleted: boolean }>(({ isCompleted = false }) => ({
+    display: "flex",
+    backgroundColor: "#fff",
+    borderRadius: "8px",
+    // padding: "16px",
+    boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+
+    ".calender-card": {
+        margin: "auto 8px",
+    },
+    ".task-info": {
+        flexGrow: 1,
         display: "flex",
-        backgroundColor: "#fff",
-        borderRadius: "8px",
-        // padding: "16px",
-        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
+        flexDirection: "column",
+        gap: "6px",
+        // backgroundColor: "#f9f9f9",
+        padding: "4px 8px",
 
-        ".calender-card": {
-            margin: "auto 8px",
+        ">h3, >p, >div": {
+            margin: "0px",
+            fontSize: "17px",
         },
-        ".task-info": {
-            flexGrow: 1,
-            display: "flex",
-            flexDirection: "column",
-            gap: "6px",
-            // backgroundColor: "#f9f9f9",
-            padding: "4px 8px",
+        ".description": {
+            color: "#555",
+            fontSize: "15px",
+            wordSpacing: "0.3px",
+            lineHeight: "1.3",
 
-            ">h3, >p, >div": {
+            display: "-webkit-box",
+            WebkitLineClamp: 2,
+            WebkitBoxOrient: "vertical",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+        },
+        ".dates": {
+            fontSize: "14px",
+            display: "flex",
+            flexDirection: "row",
+            gap: "16px",
+            marginTop: "4px",
+
+            ">p": {
+                padding: "0px",
                 margin: "0px",
-                fontSize: "17px",
-            },
-            ".description": {
-                color: "#555",
-                fontSize: "15px",
-                wordSpacing: "0.3px",
-                lineHeight: "1.3",
+                flex: 1,
+                color: "#aaa",
 
-                display: "-webkit-box",
-                WebkitLineClamp: 2,
-                WebkitBoxOrient: "vertical",
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-            },
-            ".dates": {
-                fontSize: "14px",
-                display: "flex",
-                flexDirection: "row",
-                gap: "16px",
-                marginTop: "4px",
-
-                ">p": {
-                    padding: "0px",
-                    margin: "0px",
-                    flex: 1,
-                    color: "#aaa",
-
-                    "&:first-child": {
-                        maxWidth: "150px",
-                        color: "#888",
-                    },
+                "&:first-of-type": {
+                    maxWidth: "150px",
+                    color: "#888",
                 },
             },
         },
-        ".task-actions": {
+    },
+    ".task-actions": {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "flex-end",
+        gap: "8px",
+        width: "80px",
+        flexShrink: 0,
+
+        svg: {
+            cursor: "pointer",
+            padding: "2px",
+            fontSize: "1.6rem",
+            borderRadius: "4px",
+            position: "relative",
+
+            "&:hover": {
+                backgroundColor: "#eee",
+            },
+            "&:active": {
+                backgroundColor: "#ccc",
+                top: "1px",
+            },
+        },
+        ">.actions": {
             display: "flex",
-            flexDirection: "column",
-            justifyContent: "space-between",
-            alignItems: "flex-end",
             gap: "8px",
-            width: "80px",
-            flexShrink: 0,
+            // backgroundColor: "red",
+            margin: "8px auto",
 
-            svg: {
-                cursor: "pointer",
-                padding: "2px",
-                fontSize: "1.6rem",
-                borderRadius: "4px",
-                position: "relative",
-
-                "&:hover": {
-                    backgroundColor: "#eee",
-                },
-                "&:active": {
-                    backgroundColor: "#ccc",
-                    top: "1px",
-                },
+            ".edit": {
+                color: "#1976d2",
             },
-            ">.actions": {
-                display: "flex",
-                gap: "8px",
-                // backgroundColor: "red",
-                margin: "8px auto",
 
-                ".edit": {
-                    color: "#1976d2",
-                },
-
-                ".delete": {
-                    color: "#d32f2f",
-                },
-            },
-            ".space": {
-                flexGrow: 1,
-            },
-            ".status": {
-                display: "flex",
-                alignItems: "center",
-                fontWeight: 500,
-                gap: "4px",
-                margin: "8px 0px",
-                marginRight: "12px",
-                ...(isCompeleted && { color: "#4caf50" }),
+            ".delete": {
+                color: "#d32f2f",
             },
         },
-    }),
-);
+        ".space": {
+            flexGrow: 1,
+        },
+        ".status": {
+            display: "flex",
+            alignItems: "center",
+            fontWeight: 500,
+            gap: "4px",
+            margin: "8px 0px",
+            marginRight: "12px",
+            ...(isCompleted && { color: "#4caf50" }),
+        },
+    },
+}));
